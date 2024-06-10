17ae25592d5b8c33f35c05e5f1f745fcd02daf30
Krzysztof Jabłoński <krzys.jablonski2003@gmail.com>
2024-06-10 16:18:09 +0200

    fix image displaying on windows and make generating series more intuitive
    Signed-off-by: Marcin Zepp <nircek-2103@protonmail.com>

--- GUIMyFrame1.cpp
+++ GUIMyFrame1.cpp
@@ -23,9 +23,9 @@ GUIMyFrame1::GUIMyFrame1(wxWindow* parent) : MyFrame1(parent) {
         ID_UPDATE_PROGRESS);
     resizeTimer.Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
         gen.setSide(m_panel1->GetSize().x);
-        Refresh();
+        Repaint();
     });
-    this->SetDoubleBuffered(true);
+    // this->SetDoubleBuffered(true);
 }
 
 void GUIMyFrame1::changeSize(wxSizeEvent& event) {
@@ -42,44 +42,58 @@ void GUIMyFrame1::drawOnPaint(wxPaintEvent& event) {
     m_textStatusBar->SetLabelText(timeText);
 }
 
+void GUIMyFrame1::Paint() {
+    if (!gen.isImageLoaded()) return;
+    wxClientDC dc(m_panel1);
+    dc.DrawBitmap(gen.getBitmap(), 0, 0, true);
+}
+
+void GUIMyFrame1::Repaint() {
+#ifdef _WIN32
+    Paint();
+#else
+    Refresh();
+#endif
+}
+
 void GUIMyFrame1::scrollChangeAxis(wxScrollEvent& event) {
     m_textAxis->SetLabel(std::to_string(m_sliderAxis->GetValue()));
     gen.setAxis(m_sliderAxis->GetValue());
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::scrollChangeAngle(wxScrollEvent& event) {
     m_textAngle->SetLabel(std::to_string(m_sliderAngle->GetValue()));
     gen.setAngle(m_sliderAngle->GetValue());
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::scrollChangeTranslateX(wxScrollEvent& event) {
     m_textTranslateX->SetLabel(std::to_string(m_sliderTranslateX->GetValue()));
     gen.setTranslateX(m_sliderTranslateX->GetValue() / 100.0);
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::scrollChangeTranslateY(wxScrollEvent& event) {
     m_textTranslateY->SetLabel(std::to_string(m_sliderTranslateY->GetValue()));
     gen.setTranslateY(m_sliderTranslateY->GetValue() / 100.0);
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::choiceChangeInterpolator(wxCommandEvent& event) {
     gen.setInterpolation(Kaleidoscope::InterpolationMethod(
         m_choiceInterpolator->GetSelection()));
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::choiceChangeAutoUpdate(wxCommandEvent& event) {
     auto_update = m_checkBoxAutoUpdate->GetValue();
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::choiceChangeDrawAxis(wxCommandEvent& event) {
     gen.setDrawAxis(m_checkBoxDrawAxis->GetValue());
-    if (auto_update) Refresh();
+    if (auto_update) Repaint();
 }
 
 void GUIMyFrame1::clickLoadImage(wxCommandEvent& event) {
@@ -92,7 +106,7 @@ void GUIMyFrame1::clickLoadImage(wxCommandEvent& event) {
     resetOptions();
     wxString imagePath = openFileDialog.GetPath();
     gen.setImage(wxImage(imagePath));
-    Refresh();
+    Repaint();
 }
 
 void GUIMyFrame1::clickSaveSeries(wxCommandEvent& event) {
@@ -106,7 +120,6 @@ void GUIMyFrame1::clickSaveSeries(wxCommandEvent& event) {
         if (dirDialog.ShowModal() == wxID_CANCEL) return;
 
         wxString directoryPath = dirDialog.GetPath();
-
         SeriesWorkerThread* thread =
             new SeriesWorkerThread(this, directoryPath, window.config);
         if (thread->Run() != wxTHREAD_NO_ERROR) {
@@ -142,7 +155,7 @@ void GUIMyFrame1::clickSave(wxCommandEvent& event) {
     }
 }
 
-void GUIMyFrame1::clickDraw(wxCommandEvent& event) { Refresh(); }
+void GUIMyFrame1::clickDraw(wxCommandEvent& event) { Repaint(); }
 
 void GUIMyFrame1::resetOptions() {
     gen.resetParameters();
@@ -165,20 +178,22 @@ bool GUIMyFrame1::ensureImageLoaded() {
 
 void GUIMyFrame1::generateSeries(wxString path, Config config) {
     constexpr size_t n = 61;
-    auto img = gen.getOriginalImage();
+    wxImage img = gen.getOriginalImage();
     Kaleidoscope gen;
     // TODO: add to dialog width of images: gen.setSide(...);
     // TODO: add to dialog interpolator and drawAxis
-    gen.setImage(img);
+    gen.setAxis(m_sliderAxis->GetValue());
+    gen.setSide(img.GetWidth());
+    gen.setImage(img.Copy());
     gen.setInterpolation(Kaleidoscope::InterpolationMethod(
         m_choiceInterpolator->GetSelection()));
     gen.setDrawAxis(m_checkBoxDrawAxis->GetValue());
-    double dx = std::abs(config.dx2 - config.dx1) / (n - 1);
-    double dy = std::abs(config.dy2 - config.dy1) / (n - 1);
+    double dx = std::abs(config.dx2 / 100.0 - config.dx1 / 100.0) / (n - 1);
+    double dy = std::abs(config.dy2 / 100.0 - config.dy1 / 100.0) / (n - 1);
     double dphi = std::abs(config.phi2 - config.phi1) / (n - 1);
     for (int i = 0; i < n; i++) {
-        gen.setTranslateX(config.dx1 + dx * i);
-        gen.setTranslateY(config.dy1 + dy * i);
+        gen.setTranslateX(config.dx1 / 100.0 + dx * i);
+        gen.setTranslateY(config.dy1 / 100.0 + dy * i);
         gen.setAngle(config.phi1 + dphi * i);
         gen.getImage().SaveFile(
             path + std::filesystem::path::preferred_separator +
